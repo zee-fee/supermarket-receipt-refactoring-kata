4a34ccce6db90cd29502e65e4e75c29e093bb65e
Zeenat Farheen <zeenat.farheen@rio.cloud>
2025-08-10 16:44:45 +0200

    complete test for tenpercent

--- kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
+++ kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
@@ -1,27 +1,30 @@
 package dojo.supermarket.model
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import supermarket.model.*
 
 class SupermarketTest {
 
     @Test
-    fun testSomething() {
+    fun tenPercentDiscount() {
         val catalog = FakeCatalog()
         val toothbrush = Product("toothbrush", ProductUnit.Each)
         catalog.addProduct(toothbrush, 0.99)
         val apples = Product("apples", ProductUnit.Kilo)
         catalog.addProduct(apples, 1.99)
 
-        val cart =
-            ShoppingCart()
-        cart.addItemQuantity(apples, 2.5)
+        val cart = ShoppingCart()
+        cart.addItemQuantity(toothbrush, 1.0)
+        cart.addItemQuantity(apples, 1.5)
 
         val teller = Teller(catalog)
         teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0)
 
         val receipt = teller.checksOutArticlesFrom(cart)
 
-        // Todo: complete this test
+        assertEquals(receipt.getItems().size, 2)
+        assertEquals(receipt.totalPrice!!, 3.876,0.1)
     }
+
 }
